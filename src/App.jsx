9fea6fb3f8fe46a031ecebd4500cbbd467ed9311
shahriar-raj <1905105@ugrad.cse.buet.ac.bd>
2024-01-26 18:28:03 +0600

    Tahsin chaise

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,9 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { DownloadOutlined } from '@ant-design/icons';
 import { Button, Divider, Flex, Radio } from 'antd';
-import { people } from './cats.jsx'
 import { Login } from './Login.jsx';
 
 export default function App() {
-  const listItems = people.map(person => <li>{person.name} {person.age}</li>);
   return (
     <div className="App">
       <Login />
--- src/Login.jsx
+++ src/Login.jsx
@@ -11,18 +11,12 @@ export function Login() {
     };
     return (
         <div>
-            <Card style={{ width: "750" }}>
+            <Card id="logincard">
                 <h1>Login</h1>
                 <Form
                     name="basic"
-                    labelCol={{
-                        span: 8,
-                    }}
-                    wrapperCol={{
-                        span: 16,
-                    }}
                     style={{
-                        maxWidth: 600,
+                        maxWidth: 800,
                     }}
                     initialValues={{
                         remember: false,
@@ -47,6 +41,7 @@ export function Login() {
                     <Form.Item
                         label="Password"
                         name="password"
+                        style={{ width: 400 }}
                         rules={[
                             {
                                 required: true,
@@ -60,21 +55,13 @@ export function Login() {
                     <Form.Item
                         name="remember"
                         valuePropName="checked"
-                        wrapperCol={{
-                            offset: 8,
-                            span: 16,
-                        }}
                     >
                         <Checkbox>Remember me</Checkbox>
                     </Form.Item>
 
                     <Form.Item
-                        wrapperCol={{
-                            offset: 8,
-                            span: 16,
-                        }}
                     >
-                        <Button type="primary" htmlType="submit">
+                        <Button type="primary" htmlType="submit" >
                             Submit
                         </Button>
                     </Form.Item>
