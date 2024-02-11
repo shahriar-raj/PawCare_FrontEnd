1eef94f0c8bb49fc09c3b5a77bc1341020dd03da
tahsin <1905115@ugrad.cse.buet.ac.bd>
2024-02-11 16:54:33 +0600

    Forum added from tahsin4 - 4

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import { ShowImage } from './ShowImage.jsx';
 import { DonationPayment } from './DonationPayment.jsx';
 import { AddpetPhoto } from './AddpetPhoto.jsx';
 import { DonationApply } from './DonationApply.jsx';
+import { Admin } from './Admin.jsx';
 
 export default function App() {
   const [data, setData] = useState([]);
@@ -35,7 +36,8 @@ export default function App() {
           <Route path="/donationpayment" element={<DonationPayment data={data} setData={setData} />} />   
           <Route path="/showimage" element={<ShowImage data={data} setData={setData} />} />   
           <Route path="/petPhoto" element={<AddpetPhoto data={data} setData={setData} />} />    
-          <Route path="/donationapply" element={<DonationApply data={data} setData={setData} />} />      
+          <Route path="/donationapply" element={<DonationApply data={data} setData={setData} />} />   
+          <Route path="/admin" element={<Admin data={data} setData={setData} />} />
           <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
       </Router>
